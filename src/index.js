ea9c969496bfcae0d708ffc2fbabcffa761c0979
MKS-01 <manish10mks@gmail.com>
2020-05-29 02:03:36 +0530

    resolvers

--- src/index.js
+++ src/index.js
@@ -6,29 +6,54 @@ const app = express();
 
 app.use(cors());
 
-let users = {
-  1: {
-    id: '1',
-    username: 'MKS',
-  },
-  2: {
-    id: '2',
-    username: 'MKS123',
-  },
-};
-
 const schema = gql`
   type Query {
     users: [User!]
     user(id: ID!): User
     me: User
+
+    messages: [Message!]!
+    message(id: ID!): Message!
   }
 
   type User {
     id: ID!
     username: String!
+    messages: [Message!]
+  }
+
+  type Message {
+    id: ID!
+    text: String!
+    user: User!
   }
 `;
+
+let users = {
+  1: {
+    id: '1',
+    username: 'MKS',
+    messageIds: [1],
+  },
+  2: {
+    id: '2',
+    username: 'MKS123',
+    messageIds: [2],
+  },
+};
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2',
+  },
+};
 
 const me = users[1];
 
@@ -40,8 +65,34 @@ const resolvers = {
     user: (parent, { id }) => {
       return users[id];
     },
-    me: () => {
+    me: (parent, args, { me }) => {
       return me;
+    },
+    messages: () => {
+      return Object.values(messages);
+    },
+    message: (parent, { id }) => {
+      return messages[id];
+    },
+  },
+  // User: {
+  //   // username: (user) => `${user.firstname} ${user.lastname}`,
+  //   username: (user) => {
+  //     return user.username;
+  //   },
+  // },
+
+  User: {
+    messages: (user) => {
+      return Object.values(messages).filter(
+        (message) => message.userId === user.id
+      );
+    },
+  },
+
+  Message: {
+    user: (message) => {
+      return users[message.userId];
     },
   },
 };
@@ -49,6 +100,9 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
+  context: {
+    me: users[1],
+  },
 });
 
 server.applyMiddleware({ app, path: '/graphql' });
