11b91678ac552d0f0fa66af8f2b518923ffb29ca
MKS-01 <manish10mks@gmail.com>
2020-06-01 17:00:12 +0530

    CONNECTING RESOLVERS AND DATABASE

--- src/index.js
+++ src/index.js
@@ -15,20 +15,55 @@ app.use(cors());
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
-  context: {
+  context: async () => ({
     models,
-    me: models.users[1],
-  },
+    me: await models.User.findByLogin('rwieruch'),
+  }),
 });
 
 server.applyMiddleware({ app, path: '/graphql' });
 
-// app.listen({ port: 8000 }, () => {
-//   console.log('🚀 Apollo Server on http://localhost:8000/graphql');
-// });
+const eraseDatabaseOnSync = true;
 
-sequelize.sync().then(async () => {
+sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
+  if (eraseDatabaseOnSync) {
+    createUsersWithMessages();
+  }
+
   app.listen({ port: 8000 }, () => {
-    console.log('🚀Apollo Server on http://localhost:8000/graphql');
+    console.log('🚀 Apollo Server on http://localhost:8000/graphql');
   });
 });
+
+const createUsersWithMessages = async () => {
+  await models.User.create(
+    {
+      username: 'rwieruch',
+      messages: [
+        {
+          text: 'Published the Road to learn React',
+        },
+      ],
+    },
+    {
+      include: [models.Message],
+    }
+  );
+
+  await models.User.create(
+    {
+      username: 'ddavids',
+      messages: [
+        {
+          text: 'Happy to release ...',
+        },
+        {
+          text: 'Published a complete ...',
+        },
+      ],
+    },
+    {
+      include: [models.Message],
+    }
+  );
+};
