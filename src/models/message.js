497fbdc0aeb671f44c696a7b0f8b6d8d9529b39a
MKS-01 <manish10mks@gmail.com>
2020-06-02 18:54:04 +0530

    validation

--- src/models/message.js
+++ src/models/message.js
@@ -2,6 +2,13 @@ const message = (sequelize, DataTypes) => {
   const Message = sequelize.define('message', {
     text: {
       type: DataTypes.STRING,
+
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: 'A message has to have a text.',
+        },
+      },
     },
   });
 
--- src/resolvers/user.js
+++ src/resolvers/user.js
@@ -7,6 +7,9 @@ export default {
       return await models.User.findByPk(id);
     },
     me: async (parent, args, { models, me }) => {
+      if (!me) {
+        return null;
+      }
       return await models.User.findByPk(me.id);
     },
   },
