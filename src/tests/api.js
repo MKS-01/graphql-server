1030c8506c511bc9ebcf38121fcded688b369995
MKS-01 <manish10mks@gmail.com>
2020-06-04 21:46:35 +0530

    e2e testing

--- src/tests/api.js
+++ src/tests/api.js
@@ -16,3 +16,33 @@ export const user = async (variables) =>
     `,
     variables,
   });
+
+export const signIn = async (variables) =>
+  await axios.post(API_URL, {
+    query: `
+      mutation ($login: String!, $password: String!) {
+        signIn(login: $login, password: $password) {
+          token
+        }
+      }
+    `,
+    variables,
+  });
+
+export const deleteUser = async (variables, token) =>
+  axios.post(
+    API_URL,
+    {
+      query: `
+        mutation ($id: ID!) {
+          deleteUser(id: $id)
+        }
+      `,
+      variables,
+    },
+    {
+      headers: {
+        'x-token': token,
+      },
+    }
+  );
--- src/tests/user.spec.js
+++ src/tests/user.spec.js
@@ -18,5 +18,38 @@ describe('users', () => {
 
       expect(result.data).to.eql(expectedResult);
     });
+
+    it('returns null when user cannot be found', async () => {
+      const expectedResult = {
+        data: {
+          user: null,
+        },
+      };
+
+      const result = await userApi.user({ id: '42' });
+
+      expect(result.data).to.eql(expectedResult);
+    });
+  });
+
+  describe('deleteUser(id: String!): Boolean!', () => {
+    it('returns an error because only admins can delete a user', async () => {
+      const {
+        data: {
+          data: {
+            signIn: { token },
+          },
+        },
+      } = await userApi.signIn({
+        login: 'abc',
+        password: 'abcxyz123',
+      });
+
+      const {
+        data: { errors },
+      } = await userApi.deleteUser({ id: '1' }, token);
+
+      expect(errors[0].message).to.eql('Not authorized as admin.');
+    });
   });
 });
