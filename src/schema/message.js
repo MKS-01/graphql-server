b6dee9ff1a2105edb1ff1bb3be691b18e9b02f98
MKS-01 <manish10mks@gmail.com>
2020-06-04 18:05:11 +0530

    pagination

--- src/schema/message.js
+++ src/schema/message.js
@@ -2,13 +2,23 @@ import { gql } from 'apollo-server-express';
 
 export default gql`
   extend type Query {
-    messages(cursor: String, limit: Int): [Message!]!
+    messages(cursor: String, limit: Int): MessageConnection!
     message(id: ID!): Message!
   }
 
   extend type Mutation {
     createMessage(text: String!): Message!
     deleteMessage(id: ID!): Boolean!
+  }
+
+  type MessageConnection {
+    edges: [Message!]!
+    pageInfo: PageInfo!
+  }
+
+  type PageInfo {
+    hasNextPage: Boolean!
+    endCursor: String!
   }
 
   type Message {
